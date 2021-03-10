29a4eaea098bf7bf761697ca806be63e6531656f
ashkeep <1393067551@qq.com>
2021-03-10 20:15:06 +0800

    自动化部署测试

---
+++ testaotopublish/src/main/java/com/itlikun/testaotopublish/TestaotopublishApplication.java
@@ -0,0 +1,13 @@
+package com.itlikun.testaotopublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestaotopublishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestaotopublishApplication.class, args);
+    }
+
+}
---
+++ testaotopublish/src/test/java/com/itlikun/testaotopublish/TestaotopublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlikun.testaotopublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestaotopublishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
