a6666f8082b9cbbd654b4f029a55b394c52fa9bc
ashkeep <1393067551@qq.com>
2021-03-10 21:17:50 +0800

    111

---
+++ src/main/java/com/example/testaotopublish/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.testaotopublish.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String Test(){
+        return "hello world";
+    }
+
+
+
+
+}
