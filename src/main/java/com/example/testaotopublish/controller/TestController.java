12de1758ae2b0b3f8a2acda6ab1ced274a919749
ashkeep <1393067551@qq.com>
2021-03-10 21:26:05 +0800

    1113

--- src/main/java/com/example/testaotopublish/controller/TestController.java
+++ src/main/java/com/example/testaotopublish/controller/TestController.java
@@ -13,7 +13,7 @@ public class TestController {
     @RequestMapping("/hello")
     @ResponseBody
     public String Test(){
-        return "hello world";
+        return "hello worldhh4444h";
     }
 
 
